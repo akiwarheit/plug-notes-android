45fc6e70e5d7a6a5fcc5f7671009718cbcf96f42
Kevin Jude Deloria <akiwarheit@gmail.com>
2012-02-29 18:02:26 +0800

    removed unused classes

--- src/com/plug/intents/ColorPickerIntents.java
+++
@@ -1,6 +0,0 @@
-//package com.plug.intents;
-//
-//public final class ColorPickerIntents {
-//	public final static String ACTION_PICK_COLOR = "com.drawdemo.action.PICK_COLOR";
-//	public final static String EXTRA_COLOR = "com.drawdemo.extra.COLOR";
-//}
